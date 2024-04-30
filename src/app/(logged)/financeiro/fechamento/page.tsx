039e1ab030a2c8f335be82ac02cfe1fd0b28d9a1
Marcos Temp de Souto <kikitotemp14@gmail.com>
2024-04-30 11:53:21 -0300

    fechamento quase pronto

--- src/app/(logged)/financeiro/fechamento/page.tsx
+++ src/app/(logged)/financeiro/fechamento/page.tsx
@@ -7,14 +7,20 @@ import { useCallback, useEffect, useState } from "react";
 import { ListMensalResponseType } from "@/http/parses/mensal";
 import { axiosInstance } from "@/http/config/axiosConfig";
 import dayjs from "dayjs";
+import 'dayjs/locale/pt-br';
+import { useForm } from "react-hook-form";
 // import { mensais } from "@/mocks/mensais";
+
+
+type MensalData = any;
+
 
 export default function Home(){
     const [mensais, setMensais] = useState<ListMensalResponseType>([] as ListMensalResponseType);
     const [isLoading, setIsLoading] = useState(true);
 
 
-    
+    let mesString;
 
     const fetchMensais = useCallback(async () => {
         const DATA_CORTE = 20;
@@ -31,10 +37,11 @@ export default function Home(){
 
         // alert(JSON.stringify(mes, null, 2));
 
-       let mesString = mes.toString();
+       mesString = mes.toString();
 
         try {
             setIsLoading(true);
+            // alert(dayjs(mesString).locale('pt-br').format('MMMM-YY'))
             const data = await axiosInstance.get(`/mensais/${mesString}`);
             setMensais(data.data);
             // alert(JSON.stringify(data, null, 2));
@@ -49,8 +56,73 @@ export default function Home(){
     }, [fetchMensais]);
 
 
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors, isSubmitting },
+    } = useForm<MensalData>()
+
     function alunoPago(index : number){
 
+    }
+
+    async function enviarDadosEditar(index : number, mensalId : string) {
+        let inputDiarias = document.getElementById(`diarias${index}`) as HTMLInputElement;
+        let diarias = parseInt(inputDiarias.value);
+        let inputAtrasos = document.getElementById(`atrasos${index}`) as HTMLInputElement;
+        let atrasos = parseInt(inputAtrasos.value);
+        let inputExtras = document.getElementById(`ajuste${index}`) as HTMLInputElement;
+        let extras = parseInt(inputExtras.value);
+        let inputValor = document.getElementById(`valor${index}`) as HTMLInputElement;
+        let valor_total = parseInt(inputValor.value);
+        let inputPago = document.getElementById(`pago${index}`) as HTMLInputElement;
+        let pago = inputPago.checked;
+
+        const mensal = {
+            id : mensalId,
+            diarias,
+            atrasos,
+            extras,
+            valor_total,
+            pago
+        }
+
+        alert(JSON.stringify(mensal, null, 2))
+
+        let linha = document.getElementById(`atrasos${index}`) as HTMLElement;
+
+        try {
+            const resposta = await axiosInstance.put(`/mensais`, mensal);
+            //fetchData();
+
+        } catch (error) {
+            console.log(error);
+            alert('oi')
+
+        }
+    }
+
+    function recalcularMensal(index : number){
+        // alert('mudança')
+        let inputPlano = document.getElementById(`plano${index}`) as HTMLInputElement;
+        let plano = parseInt(inputPlano.value);
+        let inputDiarias = document.getElementById(`diarias${index}`) as HTMLInputElement;
+        let diarias = parseInt(inputDiarias.value);
+        let inputAtrasos = document.getElementById(`atrasos${index}`) as HTMLInputElement;
+        let atrasos = parseInt(inputAtrasos.value);
+        let inputAjuste = document.getElementById(`ajuste${index}`) as HTMLInputElement;
+        let ajuste = parseInt(inputAjuste.value);
+        let inputValor = document.getElementById(`valor${index}`) as HTMLInputElement;
+        
+
+        let valorTotal = plano + diarias + atrasos + ajuste;
+        inputValor.value = valorTotal.toString();
+
+
+        let linha = document.getElementById(`atrasos${index}`) as HTMLElement;
+        linha
     }
 
     return(
@@ -62,26 +134,30 @@ export default function Home(){
                 {/* <LinkButton texto="Atividades" link="financeiro/atividades" /> */}
             </Header>
             <section className={styles.tabela}>
+                <h1>{dayjs(mesString).locale('pt-br').format('MMMM-YY')}</h1>
                 <div className={styles.table}>
                 <div className={styles.header}>
-                        <h3>Nome</h3>
-                        <h3>Valor do plano</h3>
-                        <h3>Valor diárias</h3>
-                        <h3>Valor atrasos</h3>
-                        <h3>Valor total</h3>
+                        <h4 style={{marginRight: '80px'}}>Nome</h4>
+                        <h4>Plano</h4>
+                        <h4>Diárias</h4>
+                        <h4>Atrasos</h4>
+                        <h4>Ajuste</h4>
+                        <h4 style={{marginRight: '100px'}}>Total</h4>
                     </div>
                     {isLoading && mensais.length === 0 && <p>Carregando...</p>} 
                     {!isLoading && mensais.length === 0 && <p>O fechamento não aconteceu ainda, aguarde até o dia 20.</p>}
                     {mensais.map((mensal, index) => {
                         return ( 
-                        <div className={styles.linha}>
+                        <div id={`linha${index}`} onChange={() => recalcularMensal(index)} className={styles.linha}>
                             <input type="text" readOnly value={mensal.aluno.nome} />
-                            <input type="number" readOnly value={mensal.aluno.valor} />
-                            <input type="number" defaultValue={mensal.diarias} />
-                            <input type="number" defaultValue={mensal.atrasos} />
-                            {/* <h1>{mensal.extras}</h1> */}
-                            <input type="number" defaultValue={mensal.valor_total} />
-                            <h6>Pago?</h6><input type="checkbox" onChange={() => alunoPago(index)} checked={mensal.pago}/>
+                            <input id={`plano${index}`} type="number" readOnly value={mensal.aluno.valor} />
+                            <input id={`diarias${index}`} type="number" defaultValue={mensal.diarias} />
+                            <input id={`atrasos${index}`} type="number" defaultValue={mensal.atrasos} />
+                            <input id={`ajuste${index}`} type="number" defaultValue={mensal.extras} />
+                            <input id={`valor${index}`} readOnly type="number" defaultValue={mensal.valor_total} />
+                            <h6>Pago?</h6><input type="checkbox" id={`pago${index}`} onChange={() => alunoPago(index)} defaultChecked={mensal.pago} />
+                            <button onClick={() => enviarDadosEditar(index, mensal.id)} 
+                                disabled={isSubmitting}>Editar</button>
                         </div>
                         )
                     })}
--- src/app/(logged)/financeiro/taxas/page.tsx
+++ src/app/(logged)/financeiro/taxas/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             //fetchData();
         } catch (error) {
             console.log(error);
-            alert('oi')
+            // alert('oi')
 
         }
     }
