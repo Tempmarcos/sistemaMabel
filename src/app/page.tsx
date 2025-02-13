cc95a65b4fc7684bed238e90860c3fe4a91dbf24
Tempmarcos <kikitotemp14@gmail.com>
2025-02-13 14:19:06 -0300

    altera ao tela login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,54 @@
+'use client'
+import { axiosInstance } from "@/http/config/axiosConfig";
+import styles from "./login.module.css"
+import { SubmitHandler, useForm } from "react-hook-form";
+
+
+
+type LoginData = {
+    user : string;
+    senha : string;
+}
+
 export default function Login(){
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors, isSubmitting },
+    } = useForm<LoginData>()
+
+
+    const onSubmit: SubmitHandler<LoginData> = async (data) => {
+        try{
+            const resposta = await axiosInstance.post(`/auth`, data);
+
+            localStorage.setItem('token', resposta.data.token)
+        }catch(error){
+            console.log(error)
+        }
+
+    }
+
     return (
-        <main>
-            <img src="./icon.png" alt="" height={'300px'} width='400px'/>
-            <h1>Usuário:</h1>
-            <input type="text" name="" id="" />
-            <h1>Senha:</h1>
-            <input type="password" name="" id="" />
-            <button><a href="/alunos">Entrar</a></button>
-        </main>
+        <body style={{'overflow': 'hidden'}}>
+            <main className={styles.main}>
+                <div className={styles.card}>
+                    <img className={styles.img} src="./icon.png" alt=""/>
+                    <div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <h1>Usuário:</h1>
+                            <input type="text" name="" id="" />
+                            <h1>Senha:</h1>
+                            <input type="password" name="" id="" />
+                            <br />
+                            <button type="submit"><a href="/alunos">Entrar</a></button>
+                        </form>
+                    </div>
+                </div>
+            </main>
+        </body>
     )
 }
